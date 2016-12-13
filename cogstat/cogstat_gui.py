8254ba39604b4c9eb81f44af7f2c07fc8844637c
krajcsi <krajcsi@gmail.com>
2016-12-13 10:34:29 +0100

    BF context menu items were not visible when selected

--- cogstat/cogstat_gui.py
+++ cogstat/cogstat_gui.py
@@ -156,7 +156,7 @@ class StatMainWindow(QtGui.QMainWindow):
         self.welcome_text_on = True  # Used for deleting the welcome text at the first analysis
         self.output_pane.setReadOnly(True)
         self.output_pane.setOpenExternalLinks(True)
-        self.output_pane.setStyleSheet("QWidget { background-color: white; }")
+        self.output_pane.setStyleSheet("QTextBrowser { background-color: white; }")
             # Some styles use non-white background (e.g. Linux Mint 17 Mate uses gray)
         # Set default font
         #print self.output_pane.currentFont().toString()
