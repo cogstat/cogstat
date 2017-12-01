f5adfc835115e9b3ed039834f96280d9175702e7
krajcsi <krajcsi@gmail.com>
2017-12-01 15:40:29 +0100

    BF: localization

--- cogstat/cogstat_stat.py
+++ cogstat/cogstat_stat.py
@@ -406,7 +406,7 @@ def descriptives(pdf, data_measlevs, var_name):
         if data_measlev in ['int', 'unk']:
             text_result += _(u'Range: %0.*f') % (prec - 1, (np.max(data) - np.min(data))) + '\n'
         elif data_measlev == 'ord':
-            text_result += _(u'Minimum and maximum: %0.*f, %0.*f') % (prec-1, np.min(data), prec-1, np.max(data))+'\n'
+            text_result += _(u'Minimum and maximum')+': %0.*f; %0.*f' % (prec-1, np.min(data), prec-1, np.max(data))+'\n'
         text_result += u'\n'
     return text_result[:-2]
 
