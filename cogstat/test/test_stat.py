b7294d8feeeda400938e122b35b1bdfced313096
krajcsi <krajcsi@gmail.com>
2018-06-13 13:07:07 +0200

    update tests

--- cogstat/test/test_stat.py
+++ cogstat/test/test_stat.py
@@ -65,40 +65,48 @@ class CogStatTestCase(unittest.TestCase):
 
         # Int variable
         result = data.explore_variable('a', 1, 2.0)
+        #for i, res in enumerate(result): print i, res
         self.assertTrue('N of valid cases: 30' in result[2])
         self.assertTrue('N of missing cases: 0' in result[2])
-        self.assertTrue('Mean: 3.1438' in result[6])
-        self.assertTrue('Standard deviation: 3.2152' in result[6])
-        self.assertTrue('Skewness: 0.3586' in result[6])
-        self.assertTrue('Kurtosis: 0.0446' in result[6])
-        self.assertTrue('Median: 2.8545' in result[6])
-        self.assertTrue('Range: 12.784' in result[6])
+        self.assertTrue('<td>Mean</td>      <td>3.1438</td>' in result[4])
+        self.assertTrue('<td>Standard deviation</td>      <td>3.2152</td>' in result[4])
+        self.assertTrue('<td>Skewness</td>      <td>0.3586</td>' in result[4])
+        self.assertTrue('<td>Kurtosis</td>      <td>0.0446</td>' in result[4])
+        self.assertTrue('<td>Range</td>      <td>12.7840</td>' in result[4])
+        self.assertTrue('<td>Maximum</td>      <td>9.9810</td>' in result[4])
+        self.assertTrue('<td>Upper quartile</td>      <td>4.3875</td>' in result[4])
+        self.assertTrue('<td>Median</td>      <td>2.8545</td>' in result[4])
+        self.assertTrue('<td>Lower quartile</td>      <td>1.4190</td>' in result[4])
+        self.assertTrue('<td>Minimum</td>      <td>-2.8030</td>' in result[4])
         # Shapiro-Wilk normality
-        self.assertTrue('<i>W</i> = 0.959' in result[7])
-        self.assertTrue('<i>p</i> = 0.287' in result[7])
+        self.assertTrue('<i>W</i> = 0.959' in result[6])
+        self.assertTrue('<i>p</i> = 0.287' in result[6])
 
         # Population estimation and one sample t-test
-        self.assertTrue('[1.9227, 4.3649]' in result[10])  # CI of the mean
-        self.assertTrue('3.2702' in result[10])  # SD
-        self.assertTrue('t</i>(29) = 1.92' in result[12])
-        self.assertTrue('p</i> = 0.065' in result[12])
+        self.assertTrue('[1.9227, 4.3649]' in result[9])  # CI of the mean
+        self.assertTrue('3.2702' in result[9])  # SD
+        self.assertTrue('t</i>(29) = 1.92' in result[11])
+        self.assertTrue('p</i> = 0.065' in result[11])
 
         # Wilcoxon signed-rank test for non-normal interval variable
         result = data.explore_variable('b', 0, 20.0)
-        self.assertTrue('T</i> = 203' in result[12])
-        self.assertTrue('p</i> = 0.551' in result[12])
+        self.assertTrue('T</i> = 203' in result[11])
+        self.assertTrue('p</i> = 0.551' in result[11])
 
         # Ord variable
         data.data_measlevs['a'] = 'ord'
         result = data.explore_variable('a', 1, 2.0)
         self.assertTrue('N of valid cases: 30' in result[2])
         self.assertTrue('N of missing cases: 0' in result[2])
-        self.assertTrue('Median: 2.8545' in result[6])
-        self.assertTrue('Minimum and maximum: -2.803; 9.981' in result[6])
+        self.assertTrue('<td>Maximum</td>      <td>9.9810</td>' in result[4])
+        self.assertTrue('<td>Upper quartile</td>      <td>4.3875</td>' in result[4])
+        self.assertTrue('<td>Median</td>      <td>2.8545</td>' in result[4])
+        self.assertTrue('<td>Lower quartile</td>      <td>1.4190</td>' in result[4])
+        self.assertTrue('<td>Minimum</td>      <td>-2.8030</td>' in result[4])
 
         # Wilcoxon signed-rank test
-        self.assertTrue('T</i> = 145' in result[10])
-        self.assertTrue('p</i> = 0.074' in result[10])
+        self.assertTrue('T</i> = 145' in result[9])
+        self.assertTrue('p</i> = 0.074' in result[9])
         data.data_measlevs['a'] = 'int'
 
     def test_explore_variable_pairs(self):
