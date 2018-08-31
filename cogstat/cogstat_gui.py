77795fe48b93eb86a615420e2b3bf9cda3501c52
krajcsi <krajcsi@gmail.com>
2018-08-31 11:37:57 +0200

    add quick start tutorial link to welcome message

--- cogstat/cogstat_gui.py
+++ cogstat/cogstat_gui.py
@@ -172,7 +172,7 @@ class StatMainWindow(QtGui.QMainWindow):
         self.output_pane.setText('<br><b>%s</b><br>%s<br>%s<br>%s<br>' %
                                  (_('Welcome to CogStat!'), _('CogStat makes statistical analysis more simple and efficient.'),
                                   _('To start working open a data file or paste your data from a spreadsheet.'),
-                                  _('Find more information about CogStat on its <a href = "http://www.cogstat.org">webpage</a>.')))
+                                  _('Find more information about CogStat on its <a href = "https://www.cogstat.org">webpage</a> or read the <a href="https://github.com/cogstat/cogstat/wiki/Quick-Start-Tutorial">quick start tutorial.</a>')))
         self.welcome_text_on = True  # Used for deleting the welcome text at the first analysis
         self.output_pane.setReadOnly(True)
         self.output_pane.setOpenExternalLinks(True)
