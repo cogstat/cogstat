af8ba11acc9546c8d15c5ab3d9ecf02f2919a7a7
krajcsi <krajcsi@gmail.com>
2019-02-12 14:15:31 +0100

    modify default result file name

--- cogstat/cogstat_dialogs.py
+++ cogstat/cogstat_dialogs.py
@@ -28,7 +28,7 @@ def open_demo_data_file():
 
 
 def save_output():
-    return unicode(QtWidgets.QFileDialog.getSaveFileName(None, _('Save result file'), 'result.pdf', '*.pdf')[0])
+    return unicode(QtWidgets.QFileDialog.getSaveFileName(None, _('Save result file'), 'CogStat analysis result.pdf', '*.pdf')[0])
 
 # XXX self.buttonBox.Ok.setEnabled(False) # TODO how can we disable the OK button without the other?
 # TODO Some variables are CamelCase - change them
