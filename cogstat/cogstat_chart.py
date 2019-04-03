2b24fffcdacbfa880b66904f800581be14a7858d
krajcsi <krajcsi@gmail.com>
2019-04-03 17:26:51 +0200

    BF Thai text is not visible on charts

--- cogstat/cogstat_chart.py
+++ cogstat/cogstat_chart.py
@@ -47,7 +47,9 @@ theme_colors = [col['color'] for col in list(plt.rcParams['axes.prop_cycle'])]
 matplotlib.rcParams['lines.dashed_pattern'] = [6.0, 6.0]
 matplotlib.rcParams['lines.dotted_pattern'] = [1.0, 3.0]
 #print matplotlib.rcParams['axes.spines.left']
-#print matplotlib.rcParams['font.size'], matplotlib.rcParams['font.serif'], matplotlib.rcParams['font.sans-serif']
+#print(matplotlib.rcParams['font.size'], matplotlib.rcParams['font.serif'], matplotlib.rcParams['font.sans-serif'])
+if csc.language == 'th':
+    matplotlib.rcParams['font.sans-serif'][0:0] = ['Umpush', 'Loma', 'Laksaman', 'KoHo', 'Garuda']
 #print matplotlib.rcParams['axes.titlesize'], matplotlib.rcParams['axes.labelsize']
 matplotlib.rcParams['axes.titlesize'] = csc.graph_title_size # title of the charts
 matplotlib.rcParams['axes.labelsize'] = csc.graph_font_size # labels of the axis
