d383fcf4ad4ea80609387c76a0b721c02a1590a9
krajcsi <krajcsi@gmail.com>
2020-01-29 10:55:00 +0100

    update setup requirements

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ setup(name='cogstat',
     packages=find_packages(),
     include_package_data=True,
     license='GNU GPL 3',
-    install_requires=['numpy', 'pandas >= 0.14.1', 'scipy >= 0.10', 'statsmodels >= 0.9', 'scikit-posthocs',
-                      'matplotlib >= 1.2.0 !=2.0.1 != 2.0.2', 'IPython', 'Jupyter', 'savReaderWriter', 'configobj',
-                      'python-bidi']
-    # You cannot set the pyqt and R dependency here, because it is not available with pip installation
+    install_requires=['numpy', 'pandas >=0.14.1', 'scipy >=0.10', 'statsmodels >=0.9', 'scikit-posthocs',
+                      'matplotlib >=1.2.0, !=2.0.1, !=2.0.2', 'IPython', 'Jupyter', 'savReaderWriter', 'configobj',
+                      'python-bidi'],
+    extras_requires={'GUI':['PyQt5']}
+    # You cannot set the R dependency here, because it is not available with pip installation
     )
