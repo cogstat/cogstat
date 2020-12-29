e6aad5bc619dbf3c880cd2f83a402572fa2b3efe
Attila Krajcsi <krajcsi@gmail.com>
2020-12-29 13:48:30 +0100

    BF import int in an object dtype

--- cogstat/cogstat.py
+++ cogstat/cogstat.py
@@ -329,8 +329,9 @@ class CogStatData:
                 non_ascii_var_names.append(variable_name)
             if self.data_frame[variable_name].dtype == 'object':  # check only string variables
                 for ind_data in self.data_frame[variable_name]:
-                    if not(ind_data != ind_data) and not (isinstance(ind_data, (bool, datetime.date))):
-                        # if not NaN, otherwise the next condition is invalid and if not boolean or date
+                    if not(ind_data != ind_data) and not (isinstance(ind_data, (bool, int, datetime.date))):
+                        # if not NaN, otherwise the next condition is invalid
+                        # and if not boolean, etc. (int can occur in object dtype)
                         if not all(ord(char) < 128 for char in ind_data):
                             non_ascii_vars.append(variable_name)
                             break  # after finding the first non-ascii data, we can leave the variable
