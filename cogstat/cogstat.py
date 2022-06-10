77e590dc5319bb194d82584af638c3e46949a155
Tamás Szűcs <92033391+TomiSzucs@users.noreply.github.com>
2022-06-10 18:44:00 +0200

    Update cogstat/cogstat.py
    Co-authored-by: Attila Krajcsi <krajcsi@users.noreply.github.com>

--- cogstat/cogstat.py
+++ cogstat/cogstat.py
@@ -822,7 +822,7 @@ class CogStatData:
         sample_result = '<cs_h2>' + _('Sample properties') + '</cs_h2>'
         residual_title = None
         residual_graph = None
-        normality = None
+        normality = None  # Are the two variables multivariate normal distributions?
         assumptions_result = None
         if meas_lev == 'nom':
             sample_result += cs_stat.contingency_table(data, [x], [y], count=True, percent=True, margins=True)
