adf24b4591dff308c7d1d92c92cfc7420ab4257f
TomiSzucs <tamas.szucs.psychology@gmail.com>
2023-01-30 16:07:51 +0100

    Validation fixes
    Correcting ICC unit tests to match the three decimal presentation.

--- cogstat/test/validate_calculations.py
+++ cogstat/test/validate_calculations.py
@@ -689,38 +689,38 @@ Control	0.538	0.107''')
         self.assertTrue('Levene test: <i>W</i> = 0.19, <i>p</i> = .904' in result[6])
         # Results
         self.assertTrue('-0.070' in result[5])  # Sample
-        self.assertTrue('<td>-0.070</td>      <td>[-0.17, 0.09]</td>' in result[8])  # Population
+        self.assertTrue('<td>-0.070</td>      <td>[-0.170, 0.090]</td>' in result[8])  # Population
         self.assertTrue('<i>F</i>(29, 90) = 0.74, <i>p</i> = .820' in result[9])  # Hypothesis test
 
         # ICC2,1 and CI
         # Results
         self.assertTrue('-0.034' in result[5])  # Sample
-        self.assertTrue('<td>-0.034</td>      <td>[-0.13, 0.12]</td>' in result[8])  # Population
+        self.assertTrue('<td>-0.034</td>      <td>[-0.130, 0.120]</td>' in result[8])  # Population
         self.assertTrue('<i>F</i>(29, 87) = 0.85, <i>p</i> = .682' in result[9])  # Hypothesis test
 
         # ICC3,1 and CI
         # Results
         self.assertTrue('-0.039' in result[5])  # Sample
-        self.assertTrue('<td>-0.039</td>      <td>[-0.15, 0.14]</td>' in result[8])  # Population
+        self.assertTrue('<td>-0.039</td>      <td>[-0.150, 0.140]</td>' in result[8])  # Population
         self.assertTrue('<i>F</i>(29, 87) = 0.85, <i>p</i> = .682' in result[9])  # Hypothesis test
 
         # ICC1,k and CI
         result = data.reliability_interrater(var_names=['a', 'e', 'f', 'g'], ratings_averaged=True)
         # Results
         self.assertTrue('-0.352' in result[5])  # Sample
-        self.assertTrue('<td>-0.352</td>      <td>[-1.35, 0.29]</td>' in result[8])  # Population
+        self.assertTrue('<td>-0.352</td>      <td>[-1.350, 0.290]</td>' in result[8])  # Population
         self.assertTrue('<i>F</i>(29, 90) = 0.74, <i>p</i> = .820' in result[9])  # Hypothesis test
 
         # ICC2,k and CI
         # Results
         self.assertTrue('-0.149' in result[5])  # Sample
-        self.assertTrue('<td>-0.149</td>      <td>[-0.82, 0.36]</td>' in result[8])  # Population
+        self.assertTrue('<td>-0.149</td>      <td>[-0.820, 0.360]</td>' in result[8])  # Population
         self.assertTrue('<i>F</i>(29, 87) = 0.85, <i>p</i> = .682' in result[9])  # Hypothesis test
 
         # ICC3,k and CI
         # Results
         self.assertTrue('-0.176' in result[5])  # Sample
-        self.assertTrue('<td>-0.176</td>      <td>[-1.05, 0.39]</td>' in result[8])  # Population
+        self.assertTrue('<td>-0.176</td>      <td>[-1.050, 0.390]</td>' in result[8])  # Population
         self.assertTrue('<i>F</i>(29, 87) = 0.85, <i>p</i> = .682')
 
 
