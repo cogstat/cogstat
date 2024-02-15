dfce34fa22eb24f03880da790fc9355521b3f1de
krajcsi <krajcsi@gmail.com>
2024-02-15 14:26:16 +0100

    use pyqt6 for dialogs

--- cogstat/ui/translate_all_uis.py
+++ cogstat/ui/translate_all_uis.py
@@ -3,7 +3,7 @@ import os
 #print os.listdir('.')
 for file in os.listdir('.'):
     if file[-2:]=='ui':
-        command = 'pyuic5 -x %s > %s.py'%(file, file[:-3])
+        command = 'pyuic6 -x %s > %s.py'%(file, file[:-3])
         print (command)
         os.system(command)
 
