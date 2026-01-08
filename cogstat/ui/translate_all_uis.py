fa04d374e4e40cc0b2cd32998ceb042f71d6b820
krajcsi <krajcsi@gmail.com>
2026-01-08 09:42:51 +0100

    PEP8 fixes

--- cogstat/ui/translate_all_uis.py
+++ cogstat/ui/translate_all_uis.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
-#print os.listdir('.')
+# print(os.listdir('.'))
 for file in os.listdir('.'):
-    if file[-2:]=='ui':
-        command = 'pyuic6 -x %s > %s.py'%(file, file[:-3])
-        print (command)
+    if file[-2:] == 'ui':
+        command = 'pyuic6 -x %s > %s.py' % (file, file[:-3])
+        print(command)
         os.system(command)
 
